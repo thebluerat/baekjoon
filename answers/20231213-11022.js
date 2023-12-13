f8282c5e853e7c3373dc8af68ce43715a2e03fd4
soomin <riato101@naver.com>
2023-12-13 22:41:09 +0900

    20231213 정진하시오

---
+++ answers/20231213-11022.js
@@ -0,0 +1,9 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split('\n');
+const T = parseInt(input[0]);
+
+for(let i = 1; i <= T; i ++) {
+    const ab = input[i].split(' ');
+    let A = Number(ab[0]);
+    let B = Number(ab[1]);
+    console.log(`Case #${i}: ${A} + ${B} = ${A+B}`)
+}
